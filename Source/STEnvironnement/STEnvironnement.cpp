f14f70bb03c9b179073e87804ec789acdabf0f75
Gaëtan Chêne <Gaetan.Chene@divtec.ch>
2021-10-04 15:16:54 +0200

    Initial Commit

---
+++ Source/STEnvironnement/STEnvironnement.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "STEnvironnement.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, STEnvironnement, "STEnvironnement" );
---
+++ Source/STEnvironnement/STEnvironnementGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "STEnvironnementGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STENVIRONNEMENT_API ASTEnvironnementGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
